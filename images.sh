c90c35d9aa3c7df29f920f41adee1846a4a5a3df
Trent Schafer <trent.schafer@gmail.com>
2023-04-17 01:27:38 +1000

    Add db version, grab latest and remove unused jq

--- images.sh
+++ images.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
-podman pull container-registry.oracle.com/database/free:latet
+podman pull container-registry.oracle.com/database/free:23.2.0.0
+podman pull container-registry.oracle.com/database/free:latest
 podman pull container-registry.oracle.com/database/ords:23.1.0
-# registry.gitlab.com/gitlab-ci-utils/curl-jq
+podman pull container-registry.oracle.com/database/ords:latest
