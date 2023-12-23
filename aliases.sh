5444aa8b464e5c8f5189a88929c1e4cecbb4c436
Trent Schafer <trent.schafer@gmail.com>
2023-12-24 08:04:50 +1100

    Use sqlcl on host machine instead of container

--- aliases.sh
+++ aliases.sh
@@ -3,5 +3,5 @@
 
 alias db23_sys_password='podman exec db bash -c '\''echo $ORACLE_PWD'\'''
 alias db23_devver_password='podman exec db bash -c '\''echo $DEVVER_PWD'\'''
-alias db23_dba='podman exec -it db bash -c '\''sqlplus sys/$ORACLE_PWD@localhost:1521/freepdb1 as sysdba'\'''
-alias db23_devver='podman exec -it db bash -c '\''sqlplus devver/$DEVVER_PWD@localhost:1521/freepdb1'\'''
+alias db23_dba='sql sys/"$(db23_sys_password)"@localhost:1521/freepdb1 as sysdba'
+alias db23_devver='sql devver/"$(db23_devver_password)"@localhost:1521/freepdb1'
