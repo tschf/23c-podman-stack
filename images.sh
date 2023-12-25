7da9563722fb5409b4af34f5d09439661296bf7b
Trent Schafer <trent.schafer@gmail.com>
2023-12-25 14:01:28 +1100

    Add grafana to pull images script

--- images.sh
+++ images.sh
@@ -9,3 +9,4 @@ podman pull container-registry.oracle.com/database/ords:latest
 # Monitoring images
 podman pull container-registry.oracle.com/database/observability-exporter:1.1.1
 podman pull quay.io/prometheus/prometheus:latest
+podman pull docker.io/grafana/grafana-oss:latest
