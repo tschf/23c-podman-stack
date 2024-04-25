4ca5dfcdef3ce945d83eac6ef8094b9a5c78087c
tschf <trent.schafer@gmail.com>
2024-04-25 20:06:34 +1000

    Set up APEX ACL (#6)

--- tearup.sh
+++ tearup.sh
@@ -166,6 +166,20 @@ begin
 end;
 /
 
+PRO Also set up ACL
+begin
+  dbms_network_acl_admin.append_host_ace(
+    host => '*',
+    ace =>
+      xs\$ace_type(
+        privilege_list => xs\$name_list('CONNECT','RESOLVE','HTTP'),
+        principal_name => apex_application.g_flow_schema_owner,
+        principal_type=> xs_acl.ptype_db
+      )
+  );
+end;
+/
+
 exit
 EOF
 
