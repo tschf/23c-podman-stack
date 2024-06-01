301908b151fd850d68a0ed326d56e1285c1e1869
Trent Schafer <trent.schafer@gmail.com>
2024-06-01 23:10:00 +1000

    New ords

--- images.sh
+++ images.sh
@@ -3,5 +3,5 @@ set -e
 
 podman pull container-registry.oracle.com/database/free:23.4.0.0
 podman pull container-registry.oracle.com/database/free:latest
-podman pull container-registry.oracle.com/database/ords:24.1.0
+podman pull container-registry.oracle.com/database/ords:24.1.1
 podman pull container-registry.oracle.com/database/ords:latest
