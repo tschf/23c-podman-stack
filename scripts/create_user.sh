72e9fe2b5c5a7e55850560dd1c98b6630ca2488e
Trent Schafer <trent.schafer@gmail.com>
2024-06-09 13:14:04 +1000

    Encrypt tablespace

--- scripts/create_user.sh
+++ scripts/create_user.sh
@@ -6,9 +6,17 @@ set -e
 
 sqlplus sys/$ORACLE_PWD@localhost:1521/FREEPDB1 as sysdba <<EOF
 
-create user devver identified by $DEVVER_PWD;
+PRO create tablespace DATA
+create tablespace data
+  datafile 'data.dbf'
+  size 100m autoextend on
+  encryption using 'AES256'
+  default storage (encrypt);
+
+PRO create user devver
+create user devver identified by $DEVVER_PWD default tablespace data;
 grant db_developer_role to devver;
-alter user devver quota unlimited on users;
+alter user devver quota unlimited on data;
 
 exit;
 EOF
