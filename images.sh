b42ac637297b0e08420672f6d5f22829eecf678f
tschf <trent.schafer@gmail.com>
2024-07-16 20:07:47 +1000

    Migrate to ords-developer image (#10)

--- images.sh
+++ images.sh
@@ -3,5 +3,5 @@ set -e
 
 podman pull container-registry.oracle.com/database/free:23.4.0.0
 podman pull container-registry.oracle.com/database/free:latest
-podman pull container-registry.oracle.com/database/ords:24.1.1
-podman pull container-registry.oracle.com/database/ords:latest
+podman pull container-registry.oracle.com/database/ords-developer:24.2.2
+podman pull container-registry.oracle.com/database/ords-developer:latest
