6c9f7edb4c19a0e3befc9390032a119648ef1dc5
Trent Schafer <trent.schafer@gmail.com>
2025-02-06 21:55:29 +1100

    Update images

--- images.sh
+++ images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-podman pull container-registry.oracle.com/database/free:23.5.0.0
+podman pull container-registry.oracle.com/database/free:23.6.0.0
 podman pull container-registry.oracle.com/database/free:latest
-podman pull container-registry.oracle.com/database/ords-developer:24.3.0
+podman pull container-registry.oracle.com/database/ords-developer:24.4.0
 podman pull container-registry.oracle.com/database/ords-developer:latest
